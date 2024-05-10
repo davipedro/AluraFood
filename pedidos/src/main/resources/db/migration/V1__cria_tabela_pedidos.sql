3f65baed257c2ca98c4ff84337f2443b858ed732
Davi Pedro <davisilvaps02@gmail.com>
2024-05-10 14:52:54 -0300

    Corrigindo migrations e conexão para compatibilidade com Postgres

--- pedidos/src/main/resources/db/migration/V1__cria_tabela_pedidos.sql
+++ pedidos/src/main/resources/db/migration/V1__cria_tabela_pedidos.sql
@@ -1,6 +1,5 @@
 CREATE TABLE pedidos (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  data_hora datetime NOT NULL,
-  status varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-)
+  id SERIAL PRIMARY KEY,
+  data_hora TIMESTAMP NOT NULL,
+  status VARCHAR(255) NOT NULL
+);
